11c273de8b0cb26c27562ab4a64ae6663f00ffab
luisponsy <bunagluisponsy@gmail.com>
2025-08-18 11:00:12 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,15 @@
+a = 1
+
+print("The value of a is:", a)
+
+a += 564548711
+print("The value of a is:", a)
+
+a -= 75477444
+print("The value of a is:", a)
+
+a *= 2
+print("The value of a is:", a)
+
+a /= 4
+print("The value of a is:", a)
