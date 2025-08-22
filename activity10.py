0925f32846cdfb8312f6e23303ba648b5a537dcf
luisponsy <bunagluisponsy@gmail.com>
2025-08-22 11:29:52 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,14 @@
+#comments 
+#using if, else yessir
+
+name = input("Input your name :")
+isStudent = input("Are you a student (Yes / No):")
+fare = eval(input("bayad:"))
+
+if isStudent.lower()== "yes":
+	discount = fare * .2
+	new_fare = fare - discount
+	print("Hi", name, "Student discount is:", discount, "Discounted fare is", new_fare)
+
+else: 
+	print("Sorry,", name,"you are not eligible for the student discount") 
