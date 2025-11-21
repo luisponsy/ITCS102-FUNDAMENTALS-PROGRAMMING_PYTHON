8fd78fc9c985782dbe270cf371d4c04c44707b0f
luisponsy <bunagluisponsy@gmail.com>
2025-11-21 11:34:06 +0800

    Add files via upload

--- code_challenge16.py
+++ code_challenge16.py
@@ -1,11 +1,12 @@
 import os
+import json
 os.system('cls')
 print("STUDENT INFORMATION SYSTEM")
 print("----------------------------\n")
 
 student_records = {}
 while True:
-    print("Select From The Options Below\nA-Add Information\nB-Print All Record\nC-Search a Record\nD-Delete a Record\nE-Modify a Record\nF-Export student\nG-Exit")
+    print("Select From The Options Below\nA-Add Information\nB-Print All Record\nC-Search a Record\nD-Delete a Record\nE-Modify a Record\nF-Export Student Data\nG-Import Student Data\nH-Exit ")
 
     choice = input("Your choice:           ").lower()
 
@@ -72,15 +73,44 @@ while True:
                 print("\nNo Existing Record.")
                 print("=====================")
         continue
-    elif choice == 'p':
-        for i,j in student_records.items():
-            print(f"Code - {i} STORED INFORMATION:f")
-        print("Editing Existing Record.")
-        continue
     elif choice == 'e':
-        print("Edit Student Record.")
+        for i,j in student_records.items():
+            search_id = input("Input ID to search:").lower()
+            print("Editing Existing Record.")
+            first_name = input("Input New Student First Name:").upper()
+            last_name = input("Input New Student Last Name:").upper()
+            course = input("Input New Student Course:").upper()
+            email = input("Input New Student E-mail Address:").upper()
+
+            student_records[student_id][0] = first_name
+            student_records[student_id][1] = last_name
+            student_records[student_id][2] = course
+            student_records[student_id][3] = email
+            ("=====================")
+            print("Data Updated.")
+            ("=====================")
+        else:
+            ("=====================")
+            print("\nNo Existing Record.")
+            print("=====================")
+        continue
     elif choice == 'f':
-        print("Export Data.")
+        os.system('cls')
+        print("Export Student Data.")
+                #file name, read / write
+        with open('student_record.json','w') as new_file:
+            json.dump(student_records, new_file, indent=4)
+        student_records = student_json
+        print("Data Exported to Json.")
     elif choice == 'g':
+        os.system('cls')
+        print("Import Student Data.")
+                #file name, read / write
+        with open('student_record.json','r') as new_file:
+            student_json = json.load(new_file)
+            student_records = student_json
+            print("Data Imported to Json.")
+        continue
+    elif choice == 'h':
         print("Exit.")
         break
